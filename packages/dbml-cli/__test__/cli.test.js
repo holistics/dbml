6deabe6e73af5d61a468511d1f03c48692d61078
phuongduyphan <phuongduyphan@gmail.com>
2019-08-26 17:25:02 +0700

    chore: add tests for dbml2sql

---
+++ packages/dbml-cli/__test__/cli.test.js
@@ -0,0 +1,36 @@
+import path from 'path';
+import fs from 'fs';
+import util from 'util';
+import childProcess from 'child_process';
+
+const exec = util.promisify(childProcess.exec);
+
+describe('@dbml/cli', () => {
+  const runTest = async (dirName) => {
+    process.chdir(dirName);
+    const args = [path.join(__dirname, 'dbml2sql_bin.js')];
+    const optsRaw = fs.readFileSync(path.join(dirName, './options.json'), 'utf-8');
+    const opts = JSON.parse(optsRaw);
+    args.push(...opts.args);
+
+    const { stdout } = await exec(`${process.execPath} ${args.join(' ')}`);
+
+    const expectStdout = fs.readFileSync(path.join(dirName, './stdout.txt'), 'utf-8');
+
+    expect(stdout).toBe(expectStdout);
+
+    if (fs.existsSync(path.join(dirName, './expect-out-files'))) {
+      const fileNames = fs.readdirSync(path.join(dirName, './out-files'));
+      let content = fs.readFileSync(path.join(dirName, './out-files', fileNames[0]), 'utf-8');
+      content = content.replace(/--.*(?:\n)*/g, '');
+      const expectContent = fs.readFileSync(path.join(dirName, './expect-out-files', fileNames[0]), 'utf-8');
+      expect(content).toBe(expectContent);
+    }
+  };
+
+  /* eslint-disable */
+  test.each(scanDirNames(__dirname, 'dbml2sql'))('dbml2sql/%s', async (dirName) => {
+    await runTest(path.join(__dirname, 'dbml2sql', dirName));
+  });
+  /* eslint-enable */
+});
