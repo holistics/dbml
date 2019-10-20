9bf633d9b8cb71b48b6737e728265f6ad6b2c6cd
duyphan <phuongduyphan@gmail.com>
2019-10-20 11:14:50 +0700

    Add export tests for composite PK

--- packages/dbml-core/__tests__/schema-exporter/postgres-exporter/output/index_tables.out.sql
+++ packages/dbml-core/__tests__/schema-exporter/postgres-exporter/output/index_tables.out.sql
@@ -1,12 +1,13 @@
 CREATE TABLE "users" (
-  "id" int PRIMARY KEY,
+  "id" int,
   "full_name" varchar,
   "email" varchar UNIQUE,
   "gender" varchar,
   "date_of_birth" varchar,
   "created_at" varchar,
   "country_code" int,
-  "active" boolean
+  "active" boolean,
+  PRIMARY KEY ("id", "full_name")
 );
 
 CREATE UNIQUE INDEX ON "users" ("id");
@@ -17,8 +18,8 @@ CREATE INDEX ON "users" USING HASH ("email", "created_at");
 
 CREATE INDEX ON "users" ((now()));
 
-CREATE INDEX ON "users" ("active", (lower(full_name)));
+CREATE INDEX ON "users" ("active", ((lower(full_name))));
 
-CREATE INDEX ON "users" ((getdate()), (upper(gender)));
+CREATE INDEX ON "users" (((getdate()), (upper(gender))));
 
 CREATE INDEX ON "users" ((reverse(country_code)));
