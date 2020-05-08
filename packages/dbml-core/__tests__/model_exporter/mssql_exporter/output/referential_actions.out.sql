9050748a43932ce772d39a0296b5b8fa2f2480f6
Minh Phan <pbminh2511@gmail.com>
2020-05-08 10:39:23 +0700

    Add support for exporting composite foreign key for mssql and change test

--- packages/dbml-core/__tests__/model_exporter/mssql_exporter/output/referential_actions.out.sql
+++ packages/dbml-core/__tests__/model_exporter/mssql_exporter/output/referential_actions.out.sql
@@ -1,21 +1,24 @@
 CREATE TABLE [orders] (
-  [id] int PRIMARY KEY IDENTITY(1, 1),
-  [user_id] int NOT NULL,
-  [created_at] datetime DEFAULT (now())
+  [id] int,
+  [cool_id] int,
+  [user_id] int UNIQUE NOT NULL,
+  [status] orders_status_enum,
+  [created_at] varchar(255),
+  PRIMARY KEY ([id], [cool_id])
 )
 GO
 
 CREATE TABLE [order_items] (
-  [id] int PRIMARY KEY IDENTITY(1, 1),
-  [order_id] int NOT NULL,
-  [product_id] int DEFAULT (null),
+  [order_id] int,
+  [cool_order_id] int,
+  [product_id] int,
   [quantity] int DEFAULT (1)
 )
 GO
 
 CREATE TABLE [products] (
   [id] int PRIMARY KEY IDENTITY(1, 1),
-  [name] nvarchar(255),
+  [name] varchar(255),
   [price] decimal(10,4),
   [created_at] datetime DEFAULT (now())
 )
@@ -23,8 +26,8 @@ GO
 
 CREATE TABLE [users] (
   [id] int PRIMARY KEY IDENTITY(1, 1),
-  [name] nvarchar(255),
-  [email] nvarchar(255) UNIQUE,
+  [name] varchar(255),
+  [email] varchar(255) UNIQUE,
   [date_of_birth] datetime,
   [created_at] datetime DEFAULT (now()),
   [country_code] int NOT NULL
@@ -33,15 +36,15 @@ GO
 
 CREATE TABLE [countries] (
   [code] int PRIMARY KEY,
-  [name] nvarchar(255),
-  [continent_name] nvarchar(255)
+  [name] varchar(255),
+  [continent_name] varchar(255)
 )
 GO
 
 ALTER TABLE [orders] ADD FOREIGN KEY ([user_id]) REFERENCES [users] ([id]) ON DELETE RESTRICT
 GO
 
-ALTER TABLE [order_items] ADD FOREIGN KEY ([order_id]) REFERENCES [orders] ([id]) ON DELETE CASCADE
+ALTER TABLE [order_items] ADD FOREIGN KEY ([order_id], [cool_order_id]) REFERENCES [orders] ([id], [cool_id]) ON DELETE CASCADE
 GO
 
 ALTER TABLE [order_items] ADD FOREIGN KEY ([product_id]) REFERENCES [products] ([id]) ON DELETE SET NULL
@@ -49,3 +52,4 @@ GO
 
 ALTER TABLE [users] ADD FOREIGN KEY ([country_code]) REFERENCES [countries] ([code]) ON DELETE NO ACTION
 GO
+
