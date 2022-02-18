d10f7024dafd4c3e5a8a2553cc8f97e52c2e0e70
SivCloud <11730168+NQPhuc@users.noreply.github.com>
2022-02-18 12:32:12 +0700

    Reorder statements in mysql exporter

---
+++ packages/dbml-core/__tests__/exporter/mysql_exporter/output/multiple_schema.out.sql
@@ -0,0 +1,46 @@
+CREATE SCHEMA `schemaB`;
+
+CREATE SCHEMA `ecommerce`;
+
+CREATE SCHEMA `schemaA`;
+
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255),
+  `pjs` ENUM ('created2', 'running2', 'done2', 'failure2'),
+  `pjs2` ENUM ('created2', 'running2', 'done2', 'failure2'),
+  `pg` ENUM ('male', 'female'),
+  `pg2` ENUM ('male2', 'female2')
+);
+
+CREATE TABLE `products` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255)
+);
+
+CREATE TABLE `ecommerce`.`users` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255),
+  `ejs` ENUM ('created2', 'running2', 'done2', 'failure2'),
+  `ejs2` ENUM ('created2', 'running2', 'done2', 'failure2'),
+  `eg` ENUM ('male', 'female'),
+  `eg2` ENUM ('male2', 'female2')
+);
+
+CREATE TABLE `schemaA`.`products` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255)
+);
+
+CREATE TABLE `schemaA`.`locations` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255)
+);
+
+ALTER TABLE `ecommerce`.`users` ADD FOREIGN KEY (`id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `ecommerce`.`users` ADD CONSTRAINT `name_optional` FOREIGN KEY (`id`) REFERENCES `users` (`name`);
+
+ALTER TABLE `schemaA`.`products` ADD FOREIGN KEY (`name`) REFERENCES `ecommerce`.`users` (`id`);
+
+ALTER TABLE `schemaA`.`locations` ADD FOREIGN KEY (`name`) REFERENCES `users` (`id`);
