f3545b8a77c4f7b8885dca6a4d9a251c4bc84431
baolequoc <bao.lq@holistics.io>
2022-08-17 12:05:24 +0700

    add schema name for junction table

--- packages/dbml-core/__tests__/exporter/mssql_exporter/output/many_to_many_relationship.out.sql
+++ packages/dbml-core/__tests__/exporter/mssql_exporter/output/many_to_many_relationship.out.sql
@@ -16,6 +16,15 @@ GO
 CREATE SCHEMA [G]
 GO
 
+CREATE SCHEMA [schema]
+GO
+
+CREATE SCHEMA [schema1]
+GO
+
+CREATE SCHEMA [schema2]
+GO
+
 CREATE TABLE [A].[a] (
   [AB] integer,
   [BA] integer,
@@ -61,14 +70,14 @@ CREATE TABLE [G].[g] (
 GO
 
 CREATE TABLE [t1] (
-  [a] int,
-  [b] int
+  [a] int PRIMARY KEY,
+  [b] int UNIQUE
 )
 GO
 
 CREATE TABLE [t2] (
-  [a] int,
-  [b] int
+  [a] int PRIMARY KEY,
+  [b] int UNIQUE
 )
 GO
 
@@ -77,7 +86,40 @@ CREATE TABLE [t1_t2] (
 )
 GO
 
-CREATE TABLE [a_b] (
+CREATE TABLE [schema].[image] (
+  [id] integer PRIMARY KEY,
+  [url] nvarchar(255)
+)
+GO
+
+CREATE TABLE [schema].[content_item] (
+  [id] integer PRIMARY KEY,
+  [heading] nvarchar(255),
+  [description] nvarchar(255)
+)
+GO
+
+CREATE TABLE [schema].[footer_item] (
+  [id] integer PRIMARY KEY,
+  [left] nvarchar(255),
+  [centre] nvarchar(255),
+  [right] nvarchar(255)
+)
+GO
+
+CREATE TABLE [schema1].[customers] (
+  [id] integer PRIMARY KEY,
+  [full_name] nvarchar(255)
+)
+GO
+
+CREATE TABLE [schema2].[orders] (
+  [id] integer PRIMARY KEY,
+  [total_price] integer
+)
+GO
+
+CREATE TABLE [A].[a_b] (
   [a_AB] integer,
   [a_BA] integer,
   [b_BC] integer,
@@ -86,28 +128,28 @@ CREATE TABLE [a_b] (
 );
 GO
 
-ALTER TABLE [a_b] ADD FOREIGN KEY ([a_AB], [a_BA]) REFERENCES [A].[a] ([AB], [BA]);
+ALTER TABLE [A].[a_b] ADD FOREIGN KEY ([a_AB], [a_BA]) REFERENCES [A].[a] ([AB], [BA]);
 GO
 
-ALTER TABLE [a_b] ADD FOREIGN KEY ([b_BC], [b_CB]) REFERENCES [B].[b] ([BC], [CB]);
+ALTER TABLE [A].[a_b] ADD FOREIGN KEY ([b_BC], [b_CB]) REFERENCES [B].[b] ([BC], [CB]);
 GO
 
 
-CREATE TABLE [d_c] (
+CREATE TABLE [D].[d_c] (
   [d_DE] integer,
   [c_CD] integer,
   PRIMARY KEY ([d_DE], [c_CD])
 );
 GO
 
-ALTER TABLE [d_c] ADD FOREIGN KEY ([d_DE]) REFERENCES [D].[d] ([DE]);
+ALTER TABLE [D].[d_c] ADD FOREIGN KEY ([d_DE]) REFERENCES [D].[d] ([DE]);
 GO
 
-ALTER TABLE [d_c] ADD FOREIGN KEY ([c_CD]) REFERENCES [C].[c] ([CD]);
+ALTER TABLE [D].[d_c] ADD FOREIGN KEY ([c_CD]) REFERENCES [C].[c] ([CD]);
 GO
 
 
-CREATE TABLE [e_g] (
+CREATE TABLE [E].[e_g] (
   [e_EF] integer,
   [e_FE] integer,
   [g_GH] integer,
@@ -116,10 +158,10 @@ CREATE TABLE [e_g] (
 );
 GO
 
-ALTER TABLE [e_g] ADD FOREIGN KEY ([e_EF], [e_FE]) REFERENCES [E].[e] ([EF], [FE]);
+ALTER TABLE [E].[e_g] ADD FOREIGN KEY ([e_EF], [e_FE]) REFERENCES [E].[e] ([EF], [FE]);
 GO
 
-ALTER TABLE [e_g] ADD FOREIGN KEY ([g_GH], [g_HG]) REFERENCES [G].[g] ([GH], [HG]);
+ALTER TABLE [E].[e_g] ADD FOREIGN KEY ([g_GH], [g_HG]) REFERENCES [G].[g] ([GH], [HG]);
 GO
 
 
@@ -148,5 +190,33 @@ ALTER TABLE [t1_t2(2)] ADD FOREIGN KEY ([t1_b]) REFERENCES [t1] ([b]);
 GO
 
 ALTER TABLE [t1_t2(2)] ADD FOREIGN KEY ([t2_b]) REFERENCES [t2] ([b]);
+GO
+
+
+CREATE TABLE [schema].[image_content_item] (
+  [image_id] integer,
+  [content_item_id] integer,
+  PRIMARY KEY ([image_id], [content_item_id])
+);
+GO
+
+ALTER TABLE [schema].[image_content_item] ADD FOREIGN KEY ([image_id]) REFERENCES [schema].[image] ([id]);
+GO
+
+ALTER TABLE [schema].[image_content_item] ADD FOREIGN KEY ([content_item_id]) REFERENCES [schema].[content_item] ([id]);
+GO
+
+
+CREATE TABLE [schema1].[customers_orders] (
+  [customers_id] integer,
+  [orders_id] integer,
+  PRIMARY KEY ([customers_id], [orders_id])
+);
+GO
+
+ALTER TABLE [schema1].[customers_orders] ADD FOREIGN KEY ([customers_id]) REFERENCES [schema1].[customers] ([id]);
+GO
+
+ALTER TABLE [schema1].[customers_orders] ADD FOREIGN KEY ([orders_id]) REFERENCES [schema2].[orders] ([id]);
 GO
 
