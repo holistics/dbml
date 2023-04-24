4dec791454b997476612058393585e8b60413430
huyphung1602 <huy.phung.sw@gmail.com>
2023-04-24 15:23:38 +0700

    Deployed to new domain dbml.dbdiagram.io

--- dbml-homepage/deploy.sh
+++ dbml-homepage/deploy.sh
@@ -10,7 +10,8 @@ npm run build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-echo 'www.dbml.org' > CNAME
+# echo 'www.dbml.org' > CNAME
+echo 'www.dbml.dbdiagram.io' > CNAME
 
 cp "home/index.html" "./"
 
