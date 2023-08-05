3940463795aaa784e341c88d37567d0079ca7728
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-08-06 00:36:40 +0700

    fix: no longer export report in interface

--- packages/dbml-core/src/parse/dbml/src/index.ts
+++ packages/dbml-core/src/parse/dbml/src/index.ts
@@ -1,8 +1,7 @@
 import Lexer from './lib/lexer/lexer';
 import Parser from './lib/parser/parser';
-import Result from './lib/result';
 import { ParsingError } from './lib/errors';
 
 export {
- Lexer, Parser, Result, ParsingError,
+ Lexer, Parser, ParsingError,
 };
