e31c7469a9b2741c772fb55889b755bec3ab1b1f
huydna <huy.dna@holistics.io>
2023-08-16 16:32:01 +0700

    feat: add symbolIndex

---
+++ packages/dbml-core/src/parse/dbml/src/lib/analyzer/symbol/symbolIndex.ts
@@ -0,0 +1,39 @@
+// Used to index a symbol table to obtain a symbol
+export type NodeSymbolId = string;
+
+export function createSchemaSymbolId(key: string): NodeSymbolId {
+  return `Schema:${key}`;
+}
+
+export function createTableSymbolId(key: string): NodeSymbolId {
+  return `Table:${key}`;
+}
+
+export function createColumnSymbolId(key: string): NodeSymbolId {
+  return `Column:${key}`;
+}
+
+export function createEnumSymbolId(key: string): NodeSymbolId {
+  return `Enum:${key}`;
+}
+
+export function createEnumElementSymbolId(key: string): NodeSymbolId {
+  return `Enum field:${key}`;
+}
+
+export function createTableGroupSymbolId(key: string): NodeSymbolId {
+  return `TableGroup:${key}`;
+}
+
+export function TableGroupElementSymbolId(key: string): NodeSymbolId {
+  return `Tablegroup field:${key}`;
+}
+
+export function destructureId(id: NodeSymbolId): { name: string; type: string } {
+  const [type, name] = id.split(':');
+
+  return {
+    name,
+    type,
+  };
+}
