07e29c4e6fd5c074759dc87c36fa185c362dc59a
huydna <huy.dna@holistics.io>
2023-08-20 23:22:22 +0700

    test: update tests

---
+++ packages/dbml-core/src/parse/dbml/tests/binder/binder.test.ts
@@ -0,0 +1,29 @@
+import { Analyzer, Lexer, Parser } from '../../src/index';
+import fs, { readFileSync } from 'fs';
+import path from 'path';
+import { describe, expect, it } from 'vitest';
+import { scanTestNames } from '../jestHelpers';
+import { serialize } from '../../src/lib/serialization/serialize';
+
+describe('#binder', () => {
+  const testNames = scanTestNames(path.resolve(__dirname, './input/'));
+
+  testNames.forEach((testName) => {
+    const program = readFileSync(path.resolve(__dirname, `./input/${testName}.in.dbml`), 'utf-8');
+    const lexer = new Lexer(program);
+    const output = serialize(
+      lexer
+        .lex()
+        .chain((tokens) => {
+          const parser = new Parser(tokens);
+          return parser.parse();
+        })
+        .chain((ast) => {
+          const analyzer = new Analyzer(ast);
+          return analyzer.analyze();
+        }),
+    );
+    it('should equal snapshot', () =>
+      expect(output).toMatchFileSnapshot(path.resolve(__dirname, `./output/${testName}.out.json`)));
+  });
+});
