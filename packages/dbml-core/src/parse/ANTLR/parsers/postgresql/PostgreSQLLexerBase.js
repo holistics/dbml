5f5183bb523e5260d4efe04a61e27aaf66bdfde7
NQPhuc <11730168+NQPhuc@users.noreply.github.com>
2023-08-21 12:46:36 +0700

    Remove useless constructor

--- packages/dbml-core/src/parse/ANTLR/parsers/postgresql/PostgreSQLLexerBase.js
+++ packages/dbml-core/src/parse/ANTLR/parsers/postgresql/PostgreSQLLexerBase.js
@@ -1,12 +1,8 @@
-/* eslint-disable no-useless-constructor */
 import antlr4 from 'antlr4';
 
 // original (C#): https://github.com/antlr/grammars-v4/blob/master/sql/postgresql/CSharp/PostgreSQLLexerBase.cs
 // Base class required by the generated lexer
 export default class PostgreSQLLexerBase extends antlr4.Lexer {
-  constructor (input) {
-    super(input);
-  }
 }
 
 // helper functions required by the generated grammar
--- packages/dbml-core/src/parse/ANTLR/parsers/postgresql/PostgreSQLParserBase.js
+++ packages/dbml-core/src/parse/ANTLR/parsers/postgresql/PostgreSQLParserBase.js
@@ -1,12 +1,8 @@
-/* eslint-disable no-useless-constructor */
 import antlr4 from 'antlr4';
 
 // Original (C#): https://github.com/antlr/grammars-v4/blob/master/sql/postgresql/CSharp/PostgreSQLLexerBase.cs
 // Base class required by the generated parser
 export default class PostgreSQLParserBase extends antlr4.Parser {
-  constructor (input) {
-    super(input);
-  }
 }
 
 // helper function required by the generated grammar
