e45f99207347e421d5240c5d50704249ef0e8ba1
NQPhuc <11730168+NQPhuc@users.noreply.github.com>
2023-08-21 18:02:27 +0700

    Update postgres issue tests. Fix identifier visit

--- packages/dbml-core/__tests__/importer/postgres_importer/input/issue427.in.sql
+++ packages/dbml-core/__tests__/importer/postgres_importer/input/issue427.in.sql
@@ -3,6 +3,6 @@ CREATE TABLE public.users (
     hashed_password character varying NOT NULL,
     full_name character varying NOT NULL,
     email character varying NOT NULL,
-    password_changed_at timestamp with time zone DEFAULT '0001-01-01 00:00:00+00'::timestamp with time zone NOT NULL, -- this is line 183
+    password_changed_at timestamp with time zone DEFAULT '0001-01-01 00:00:00+00'::timestamp with time zone NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL
 );
