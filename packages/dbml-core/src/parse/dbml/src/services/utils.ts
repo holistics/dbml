d5f58de752024144011bb9419661a313905e2f1c
huydo862003 <huy.do862003@hcmut.edu.vn>
2023-10-06 00:59:52 +0700

    fix: revamp compiler & autosuggestions

--- packages/dbml-core/src/parse/dbml/src/services/utils.ts
+++ packages/dbml-core/src/parse/dbml/src/services/utils.ts
@@ -2,5 +2,5 @@ import { Position } from 'monaco-editor-core';
 import { TextModel } from './types';
 
 export function getOffsetFromMonacoPosition(model: TextModel, position: Position): number {
-  return model.getOffsetAt(position) - 1;
+  return model.getOffsetAt(position);
 }
