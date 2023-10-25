32b80a164690f56261842f9e34b43cf68806e91a
huydna <huy.dna@holistics.io>
2023-10-25 11:35:10 +0700

    test: add tests for the new parser

---
+++ packages/dbml-core/src/parse/dbml/dbmlparserv2/tests/parser/parser.test.ts
@@ -0,0 +1,27 @@
+import fs, { readFileSync } from 'fs';
+import path from 'path';
+import { describe, expect, it } from 'vitest';
+import { scanTestNames } from '../jestHelpers';
+import Lexer from '../../src/lib/lexer/lexer';
+import Parser from '../../src/lib/parser/parser';
+import { SyntaxNodeIdGenerator } from '../../src/lib/parser/nodes';
+
+describe('#parser', () => {
+  const testNames = scanTestNames(path.resolve(__dirname, './input/'));
+
+  testNames.forEach((testName) => {
+    const program = readFileSync(path.resolve(__dirname, `./input/${testName}.in.dbml`), 'utf-8');
+    const lexer = new Lexer(program);
+    const nodeIdGenerator = new SyntaxNodeIdGenerator();
+    const output = JSON.stringify(
+      lexer.lex().chain((tokens) => {
+        const parser = new Parser(tokens, nodeIdGenerator);
+        return parser.parse();
+      }),
+      null,
+      2,
+    );
+    it('should equal snapshot', () =>
+      expect(output).toMatchFileSnapshot(path.resolve(__dirname, `./output/${testName}.out.json`)));
+  });
+});
