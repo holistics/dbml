88ec6a839ce12eed575f6cfdc5ac5b1708b7c99c
huydna <huy.dna@holistics.io>
2023-10-25 16:36:46 +0700

    fix: export DBMLv2 in index.d.ts

--- packages/dbml-core/types/index.d.ts
+++ packages/dbml-core/types/index.d.ts
@@ -1,5 +1,6 @@
+import DBMLv2 from 'dbmlparserv2';
 import ModelExporter from './export/ModelExporter';
 import Parser from './parse/Parser';
 import importer from './import';
 import exporter from './export';
-export { importer, exporter, ModelExporter, Parser, };
+export { importer, exporter, ModelExporter, Parser, DBMLv2 };
