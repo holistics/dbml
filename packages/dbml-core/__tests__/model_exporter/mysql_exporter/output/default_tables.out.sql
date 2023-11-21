0419cee362f7a93624268037f13124cdc98861af
Nguyen Hoang <anhnguyen3120@gmail.com>
2023-11-21 12:25:04 +0700

    fix(MysqlExporter): use single quote when exporting default value

--- packages/dbml-core/__tests__/model_exporter/mysql_exporter/output/default_tables.out.sql
+++ packages/dbml-core/__tests__/model_exporter/mysql_exporter/output/default_tables.out.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `products` (
   `id` int PRIMARY KEY DEFAULT 123,
-  `name` varchar(255) DEFAULT "Tea",
+  `name` varchar(255) DEFAULT 'Tea',
   `merchant_id` int NOT NULL,
   `price` float DEFAULT 123.12,
   `status` varchar(255) DEFAULT NULL,
