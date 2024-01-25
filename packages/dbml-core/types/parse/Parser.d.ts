a51071f312a776932f03d290ca43edd2fe5139fb
NQPhuc <11730168+NQPhuc@users.noreply.github.com>
2024-01-25 12:34:24 +0700

    add static parse() method for compability

--- packages/dbml-core/types/parse/Parser.d.ts
+++ packages/dbml-core/types/parse/Parser.d.ts
@@ -1,7 +1,7 @@
 import { Compiler } from '@dbml/parse';
 import Database, { RawDatabase } from '../model_structure/database';
 declare class Parser {
-    constructor(DBMLCompiler: Compiler?);
+    constructor(DBMLCompiler?: Compiler);
     static parseJSONToDatabase(rawDatabase: RawDatabase): Database;
     static parseMySQLToJSON(str: string): RawDatabase;
     static parsePostgresToJSON(str: string): RawDatabase;
@@ -9,6 +9,10 @@ declare class Parser {
     static parseDBMLToJSON(str: string): RawDatabase;
     static parseSchemaRbToJSON(str: string): RawDatabase;
     static parseMSSQLToJSON(str: string): RawDatabase;
+    /**
+     * Should use the parser() instance method instead of this static method whenever possible
+     */
+    static parse(str: string, format: 'mysql' | 'postgres' | 'dbml' | 'dbmlv2' | 'schemarb' | 'mssql' | 'json'): Database;
     parse(str: string, format: 'mysql' | 'postgres' | 'dbml' | 'dbmlv2' | 'schemarb' | 'mssql' | 'json'): Database;
 }
 export default Parser;
