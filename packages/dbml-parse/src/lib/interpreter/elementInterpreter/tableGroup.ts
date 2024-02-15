812af17cc90463cb75d6ff3864146d7da5aae3b2
HuyDNA <huydo862003@gmail.com>
2024-02-15 17:29:39 +0700

    fix: improve error message

--- packages/dbml-parse/src/lib/interpreter/elementInterpreter/tableGroup.ts
+++ packages/dbml-parse/src/lib/interpreter/elementInterpreter/tableGroup.ts
@@ -59,7 +59,7 @@ export class TableGroupInterpreter implements ElementInterpreter {
         const tableGroup = this.env.tableOwnerGroup[tableid];
         const { schemaName, name } = this.env.tableGroups.get(tableGroup)!;
         const groupName = schemaName ? `${schemaName}.${name}` : name;
-        errors.push(new CompileError(CompileErrorCode.TABLE_REAPPEAR_IN_TABLEGROUP, `Table ${fragments.join('.')} already appears in group "${groupName}"`, field));
+        errors.push(new CompileError(CompileErrorCode.TABLE_REAPPEAR_IN_TABLEGROUP, `Table "${fragments.join('.')}" already appears in group "${groupName}"`, field));
       } else {
         this.env.tableOwnerGroup[tableid] = this.declarationNode;
       }
