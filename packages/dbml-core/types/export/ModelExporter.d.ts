f4e98973bcc259da85b18cd0cb10093f0fb8ad3e
Huy Le <huy.lm@holistics.io>
2024-03-12 23:05:09 +0700

    Refactor exporter ts.d files

--- packages/dbml-core/types/export/ModelExporter.d.ts
+++ packages/dbml-core/types/export/ModelExporter.d.ts
@@ -1,5 +1,7 @@
 import Database, { NormalizedDatabase } from '../model_structure/database';
+
+export type ExportFormatOptions = 'dbml' | 'mysql' | 'postgres' | 'json' | 'mssql' | 'oracle';
 declare class ModelExporter {
-    static export(model: Database | NormalizedDatabase, format: 'dbml' | 'mysql' | 'postgres' | 'json' | 'mssql' | 'oracle', isNormalized?: boolean): string;
+    static export(model: Database | NormalizedDatabase, format: ExportFormatOptions, isNormalized?: boolean): string;
 }
 export default ModelExporter;
