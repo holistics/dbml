6766439a8edd7c3beecffe6b8e64de81c2c663bb
Huy Le <huy.lm@holistics.io>
2024-03-18 12:23:10 +0700

    Update code for review

--- packages/dbml-core/types/export/ModelExporter.d.ts
+++ packages/dbml-core/types/export/ModelExporter.d.ts
@@ -1,7 +1,7 @@
 import Database, { NormalizedDatabase } from '../model_structure/database';
 
-export type ExportFormatOptions = 'dbml' | 'mysql' | 'postgres' | 'json' | 'mssql' | 'oracle';
+export type ExportFormatOption = 'dbml' | 'mysql' | 'postgres' | 'json' | 'mssql' | 'oracle';
 declare class ModelExporter {
-    static export(model: Database | NormalizedDatabase, format: ExportFormatOptions, isNormalized?: boolean): string;
+    static export(model: Database | NormalizedDatabase, format: ExportFormatOption, isNormalized?: boolean): string;
 }
 export default ModelExporter;
