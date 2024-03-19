48c6b90ef11c526ed1234a47d9fa2f0456592636
Huy Le <huy.lm@holistics.io>
2024-03-19 11:15:51 +0700

    Create exporter README and composite foreign key test

--- packages/dbml-core/__tests__/exporter/oracle_exporter/output/special_schema.out.sql
+++ packages/dbml-core/__tests__/exporter/oracle_exporter/output/special_schema.out.sql
@@ -9,7 +9,9 @@ CREATE TABLE "orders" (
   "status" nvarchar2(255) NOT NULL CHECK ("status" IN ('created', 'pending', 'processing', 'waiting_for_payment', 'success', 'cancel')),
   "note" nclob DEFAULT '',
   "user_id" integer NOT NULL,
-  "created_at" timestamp DEFAULT current_timestamp
+  "created_at" timestamp DEFAULT current_timestamp,
+  "product_id" int,
+  "supplier_id" int
 );
 
 CREATE TABLE "order_details" (
@@ -19,6 +21,12 @@ CREATE TABLE "order_details" (
   "price" float NOT NULL
 );
 
+CREATE TABLE "products" (
+  "product_id" int,
+  "supplier_id" int,
+  PRIMARY KEY ("product_id", "supplier_id")
+);
+
 CREATE INDEX "IDX_USERS" ON "users" (LOWER("fullname"), "username");
 
 CREATE UNIQUE INDEX "IDX_ORDER_USER" ON "orders" ("id", "user_id");
@@ -26,6 +34,8 @@ CREATE UNIQUE INDEX "IDX_ORDER_USER" ON "orders" ("id", "user_id");
 COMMENT ON TABLE "users" IS 'This table has a expression index';
 
 COMMENT ON COLUMN "orders"."user_id" IS 'Store user uuid';
+
+ALTER TABLE "orders" ADD FOREIGN KEY ("product_id", "supplier_id") REFERENCES "products" ("product_id", "supplier_id");
 
 ALTER TABLE "orders" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE;
 
