63fa076770cfda1566c703b9888b2058be006bb3
HuyDNA <huydo862003@gmail.com>
2024-03-21 03:15:32 +0700

    fix: define a common error format

---
+++ packages/dbml-core/types/parse/error.d.ts
@@ -0,0 +1,28 @@
+export declare type WarningLevel =
+  | 'error'
+  | 'warning'
+  | 'info';
+
+export declare type ErrorCode = number;
+
+export declare interface EditorPosition {
+  line: number;
+  column: number;
+}
+
+export interface CompilerDiagnostic {
+  readonly message: Readonly<string>;
+  readonly location: {
+      start: Readonly<EditorPosition>,
+      // in monaco, if an end position is not specified
+      // it consumes the word containing the start position
+      end?: Readonly<EditorPosition>,
+  };
+  readonly type?: Readonly<WarningLevel>;
+  readonly code?: Readonly<ErrorCode>; // the error code
+  readonly [key: string]: any;
+}
+
+export interface CompilerError {
+  readonly diags: CompilerDiagnostic[];
+}
