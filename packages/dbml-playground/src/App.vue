f13da94ffaef194a71360342695a7cfaee4e3e89
Huy-DNA <huydo862003@gmail.com>
2024-06-06 17:47:13 +0700

    chore: reset everything

--- packages/dbml-playground/src/App.vue
+++ packages/dbml-playground/src/App.vue
@@ -1,21 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
