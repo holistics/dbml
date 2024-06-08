1ef845f548f408eab0c81b4ee187a9c0b90aa15a
Huy-DNA <huydo862003@gmail.com>
2024-06-08 11:37:32 +0700

    feat: basic pane layout

--- packages/dbml-playground/src/pages/PlaygroundPage.vue
+++ packages/dbml-playground/src/pages/PlaygroundPage.vue
@@ -1,3 +1,20 @@
 <template>
-  
+  <splitpane vertical>
+    <splitpane horizontal>
+      <splitpane vertical>
+        <FileTreePane />
+        <DbmlEditorPane />
+      </splitpane>
+      <ErrorPane />
+    </splitpane>
+    <AstExplorerPane />
+  </splitpane>
 </template>
+
+<script setup lang="ts">
+  import { Splitpanes } from 'splitpanes';
+  import AstExplorerPane from '../components/AstExplorerPane/AstExplorerPane.vue'
+  import ErrorPane from '../components/ErrorPane/ErrorPane.vue'
+  import DbmlEditorPane from '../components/DbmlEditorPane/DbmlEditorPane.vue'
+  import FileTreePane from '../components/FileTreePane/FileTreePane.vue'
+</script>
