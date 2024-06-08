c4def9a919ccba7e93eac434fea8d708d6a7d907
Huy-DNA <huydo862003@gmail.com>
2024-06-08 12:10:08 +0700

    fix: basic error pane

--- packages/dbml-playground/src/components/ErrorPane/ErrorPane.vue
+++ packages/dbml-playground/src/components/ErrorPane/ErrorPane.vue
@@ -1,10 +1,24 @@
 <template>
   <pane>
     <slot>
+      <ul v-if='errors.length'>
+        <li v-for='error in errors' :key='error'>
+          <p> {{ error }} </p>
+        </li>
+      </ul>
+
+      <p v-else>
+        No errors found
+      </p>
     </slot>
   </pane>
 </template>
 
 <script setup lang="ts">
+  import { computed } from 'vue';
   import { Pane } from 'splitpanes';
+  import type { Compiler } from '@dbml/parse';
+
+  const { compiler } = defineProps<{ compiler: Compiler }>();
+  const errors = computed(() => compiler.parse.errors());
 </script>
