0dcc7bcaf9c96aa67022dae9a5ccf0618876df42
Huy-DNA <huydo862003@gmail.com>
2024-06-08 17:21:13 +0700

    feat: "No errors." message

--- packages/dbml-playground/src/components/ErrorPane/ErrorPane.vue
+++ packages/dbml-playground/src/components/ErrorPane/ErrorPane.vue
@@ -1,15 +1,16 @@
 <template>
   <pane min-size='30' max-size='30' size='30'>
     <slot>
-      <section class='h-full overflow-auto bg-dark text-white border rounded border-gray-300 flex flex-col p-2'>
+      <section class='h-full overflow-auto bg-dark border rounded border-gray-300 flex flex-col p-4 text-sm text-gray-400'>
         <ul v-if='errors.length'>
           <li v-for='error in errors' :key='error'>
             <p> {{ error }} </p>
           </li>
         </ul>
 
-        <p v-else>
-          No errors found
+        <p v-else class='font-light flex gap-2 items-center'>
+          <FontAwesomeIcon :icon='faCheckCircle' class='text-green-400'/>
+          <span>No errors.</span>
         </p>
       </section>
     </slot>
@@ -19,6 +20,8 @@
 <script setup lang="ts">
   import { computed } from 'vue';
   import { Pane } from 'splitpanes';
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+  import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
   import { useCompilerStore } from '@/stores/compiler';
 
   const compiler = useCompilerStore();
