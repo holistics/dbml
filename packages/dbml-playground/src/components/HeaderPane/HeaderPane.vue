95dbd0718c51f70533bbe37e52409d5a11dfc267
Huy-DNA <huydo862003@gmail.com>
2024-06-08 11:25:27 +0700

    fix: remove one layer of div in header pane

--- packages/dbml-playground/src/components/HeaderPane/HeaderPane.vue
+++ packages/dbml-playground/src/components/HeaderPane/HeaderPane.vue
@@ -1,10 +1,8 @@
 <template>
   <pane class='border-b-[1px] border-b-gray-300 bg-white h-16 p-4 flex justify-between' role='navigation'>
-    <div class='flex gap-2'> 
-      <div role='banner' class='flex flex-row items-center gap-2'>
-        <img :src='DbmlLogoUrl' width=24 />
-        <p class='font-bold'>dbx / DBML Playground</p>
-      </div>
+    <div role='banner' class='flex flex-row items-center gap-2'>
+      <img :src='DbmlLogoUrl' width=24 />
+      <p class='font-bold'>dbx / DBML Playground</p>
     </div>
   </pane>
 </template>
