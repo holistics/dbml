6a503ec7db2bcda6caaad6cd5ae53cc41a803eb8
Huy-DNA <huydo862003@gmail.com>
2024-06-08 10:33:54 +0700

    feat: add playground page

--- packages/dbml-playground/src/router/index.ts
+++ packages/dbml-playground/src/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PlaygroundPage from '../pages/PlaygroundPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/', name: 'DBML Playground', component: PlaygroundPage, },
   ]
 })
 
