beac1e55d9b7d68e2995d7d056fd58d5b0940dbf
Huy-DNA <huydo862003@gmail.com>
2024-06-08 10:41:48 +0700

    feat: basic header pane

--- packages/dbml-playground/src/App.vue
+++ packages/dbml-playground/src/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { Splitpanes } from 'splitpanes';
+import HeaderPane from './components/HeaderPane/HeaderPane.vue'
+
 </script>
 
 <template>
-  <RouterView />
+  <splitpanes horizontal>
+    <HeaderPane>
+      <RouterLink to="/">DBML Playground</RouterLink> 
+    </HeaderPane>
+    <RouterView />
+  </splitpanes>
 </template>
---
+++ packages/dbml-playground/src/components/HeaderPane/HeaderPane.vue
@@ -0,0 +1,12 @@
+<template>
+  <pane>
+    <img :src='DbmlLogoUrl' width=60 height=60/>
+    <slot/>
+  </pane>
+</template>
+
+<script setup lang="ts">
+  import { Pane } from 'splitpanes';
+  import DbmlLogoUrl from '../../assets/images/dbml-logo.png';
+</script>
+
