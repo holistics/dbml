9180956c64102cbb8b13b72d709fd145f0707fda
Huy-DNA <huydo862003@gmail.com>
2024-06-08 15:05:44 +0700

    fix: load editor worker

--- packages/dbml-playground/src/components/DbmlEditorPane/Editor/Editor.vue
+++ packages/dbml-playground/src/components/DbmlEditorPane/Editor/Editor.vue
@@ -5,7 +5,16 @@
 <script setup lang="ts">
   import { ref, onMounted } from 'vue';
   import * as monaco from 'monaco-editor';
-  
+  import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker.js?worker&inline';
+
+  const editorWorker = new EditorWorker();
+
+  (window as any).MonacoEnvironment = {
+    getWorker () {
+      return editorWorker;
+    },
+  };
+
   const editorDomNode = ref(null);
   const editor = ref<monaco.editor.IEditor | null>(null);
   onMounted(() => {
