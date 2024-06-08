6ec7ec377152dab0823cf4c4954c114c52af1982
Huy-DNA <huydo862003@gmail.com>
2024-06-08 17:25:45 +0700

    feat: icons for error message

--- packages/dbml-playground/src/components/ErrorPane/ErrorPane.vue
+++ packages/dbml-playground/src/components/ErrorPane/ErrorPane.vue
@@ -2,14 +2,17 @@
   <pane min-size='30' max-size='30' size='30'>
     <slot>
       <section class='h-full overflow-auto bg-dark border rounded border-gray-300 flex flex-col p-4 text-sm text-gray-400'>
-        <ul v-if='errors.length'>
+        <ul v-if='errors.length' class='list-none'>
           <li v-for='error in errors' :key='error'>
-            <p> {{ error }} </p>
+            <p class='font-light flex gap-2 items-center'>
+              <FontAwesomeIcon :icon='faExclamationCircle' class='text-red-500' />
+              {{ error }}
+            </p>
           </li>
         </ul>
 
         <p v-else class='font-light flex gap-2 items-center'>
-          <FontAwesomeIcon :icon='faCheckCircle' class='text-green-400'/>
+          <FontAwesomeIcon :icon='faCheckCircle' class='text-green-400' />
           <span>No errors.</span>
         </p>
       </section>
@@ -21,7 +24,7 @@
   import { computed } from 'vue';
   import { Pane } from 'splitpanes';
   import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-  import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+  import { faCheckCircle, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
   import { useCompilerStore } from '@/stores/compiler';
 
   const compiler = useCompilerStore();
