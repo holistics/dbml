84ee493bfc0836a75d1deef32377bb0b52ed9ef9
Huy-DNA <huydo862003@gmail.com>
2024-06-08 12:37:49 +0700

    feat: basic monaco editor setup

--- packages/dbml-playground/src/components/DbmlEditorPane/DbmlEditorPane.vue
+++ packages/dbml-playground/src/components/DbmlEditorPane/DbmlEditorPane.vue
@@ -1,10 +1,12 @@
 <template>
   <pane>
     <slot>
+      <Editor />
     </slot>
   </pane>
 </template>
 
 <script setup lang="ts">
   import { Pane } from 'splitpanes';
+  import Editor from './Editor/Editor.vue';
 </script>
---
+++ packages/dbml-playground/src/components/DbmlEditorPane/Editor/Editor.vue
@@ -0,0 +1,16 @@
+<template>
+  <div ref='editorDomNode'></div>
+</template>
+
+<script setup lang="ts">
+  import { ref, onMounted } from 'vue';
+  import * as monaco from 'monaco-editor';
+  
+  const editorDomNode = ref(null);
+  const editor = ref<monaco.editor.IEditor | null>(null);
+  onMounted(() => {
+    editor.value = monaco.editor.create(editorDomNode.value, {
+      value: '',
+    }) 
+  });
+</script>
