69e097cd595fe195365a002a772e1ae86c233d8b
Huy-DNA <huydo862003@gmail.com>
2024-06-20 14:41:42 +0700

    fix: move compiler to a folder

--- packages/dbml-parse/src/compiler/index.ts
+++ packages/dbml-parse/src/compiler/index.ts
@@ -1,9 +1,9 @@
 import { findLastIndex, last } from 'lodash';
-import { SymbolKind, destructureIndex } from './lib/analyzer/symbol/symbolIndex';
-import { generatePossibleIndexes } from './lib/analyzer/symbol/utils';
-import SymbolTable from './lib/analyzer/symbol/symbolTable';
-import { isOffsetWithinSpan } from './lib/utils';
-import { CompileError } from './lib/errors';
+import { SymbolKind, destructureIndex } from '../lib/analyzer/symbol/symbolIndex';
+import { generatePossibleIndexes } from '../lib/analyzer/symbol/utils';
+import SymbolTable from '../lib/analyzer/symbol/symbolTable';
+import { isOffsetWithinSpan } from '../lib/utils';
+import { CompileError } from '../lib/errors';
 import {
   BlockExpressionNode,
   ElementDeclarationNode,
@@ -16,17 +16,17 @@ import {
   SyntaxNode,
   SyntaxNodeIdGenerator,
   TupleExpressionNode,
-} from './lib/parser/nodes';
-import { NodeSymbol, NodeSymbolIdGenerator } from './lib/analyzer/symbol/symbols';
-import Report from './lib/report';
-import Lexer from './lib/lexer/lexer';
-import Parser from './lib/parser/parser';
-import Analyzer from './lib/analyzer/analyzer';
-import Interpreter from './lib/interpreter/interpreter';
-import { SyntaxToken, SyntaxTokenKind } from './lib/lexer/tokens';
-import { getMemberChain, isInvalidToken } from './lib/parser/utils';
-import { Database } from './lib/interpreter/types';
-import { DBMLCompletionItemProvider, DBMLDefinitionProvider, DBMLReferencesProvider } from './services/index';
+} from '../lib/parser/nodes';
+import { NodeSymbol, NodeSymbolIdGenerator } from '../lib/analyzer/symbol/symbols';
+import Report from '../lib/report';
+import Lexer from '../lib/lexer/lexer';
+import Parser from '../lib/parser/parser';
+import Analyzer from '../lib/analyzer/analyzer';
+import Interpreter from '../lib/interpreter/interpreter';
+import { SyntaxToken, SyntaxTokenKind } from '../lib/lexer/tokens';
+import { getMemberChain, isInvalidToken } from '../lib/parser/utils';
+import { Database } from '../lib/interpreter/types';
+import { DBMLCompletionItemProvider, DBMLDefinitionProvider, DBMLReferencesProvider } from '../services/index';
 
 const enum Query {
   _Interpret,
