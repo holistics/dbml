4e5d5c11f1defa5c823c596322745664b34b198e
huyphung1602 <huy.phung.sw@gmail.com>
2024-07-04 01:43:47 +0700

    Support fk (refs)

--- packages/dbml-core/src/parse/ANTLR/ASTGeneration/snowflake/SnowflakeASTGen.js
+++ packages/dbml-core/src/parse/ANTLR/ASTGeneration/snowflake/SnowflakeASTGen.js
@@ -1,4 +1,5 @@
 /* eslint-disable class-methods-use-this */
+import { flatten } from 'lodash';
 import SnowflakeParserVisitor from '../../parsers/snowflake/SnowflakeParserVisitor';
 import { Endpoint, Enum, Field, Index, Table, Ref } from '../AST';
 import { TABLE_CONSTRAINT_KIND, COLUMN_CONSTRAINT_KIND, DATA_TYPE, CONSTRAINT_TYPE } from '../constants';
@@ -51,6 +52,7 @@ export default class SnowflakeASTGen extends SnowflakeParserVisitor {
   visitCreate_command (ctx) {
     if (ctx.create_table()) {
       const table = ctx.create_table().accept(this);
+      console.log(table);
       this.data.tables.push(table);
     }
   }
@@ -62,29 +64,42 @@ export default class SnowflakeASTGen extends SnowflakeParserVisitor {
   ) ((comment_clause? create_table_clause) | (create_table_clause comment_clause?))
   */
   visitCreate_table (ctx) {
-    const [databaseName, schemaName, name] = ctx.object_name().accept(this);
+    const [databaseName, schemaName, tableName] = ctx.object_name().accept(this);
 
     const definitions = ctx.create_table_clause().accept(this);
 
-    const [fieldsData, indexes, tableRefs, singlePkIndex] = definitions.reduce((acc, ele) => {
+    const [fieldsData, indexes, tableRefs, pk] = definitions.reduce((acc, ele) => {
       if (ele.kind === TABLE_CONSTRAINT_KIND.FIELD) acc[0].push(ele.value);
-      else if (ele.kind === TABLE_CONSTRAINT_KIND.INDEX) acc[1].push(ele.value);
-      else if (ele.kind === TABLE_CONSTRAINT_KIND.UNIQUE) acc[1].push(ele.value);
-      else if (ele.kind === TABLE_CONSTRAINT_KIND.FK) acc[2].push(ele.value);
-      else if (ele.kind === TABLE_CONSTRAINT_KIND.PK) {
-        /** @type {Index} */
-        const index = ele.value;
-        if (index.columns.length > 1) acc[1].push(ele.value);
-        else acc[3] = index;
+      else if (ele.kind === TABLE_CONSTRAINT_KIND.UNIQUE) acc[1] = ele.value;
+      else if (ele.kind === TABLE_CONSTRAINT_KIND.FK) {
+        acc[2].push(ele.value);
+      } else if (ele.kind === TABLE_CONSTRAINT_KIND.PK) {
+        if (ele.value.length === 1) {
+          acc[3] = ele.value[0];
+        }
       }
       return acc;
     }, [[], [], [], null]);
 
-    return new Table({
-      name,
+    this.data.refs.push(...tableRefs.map(tableRef => {
+      tableRef.endpoints[0].tableName = tableName;
+      tableRef.endpoints[0].schemaName = schemaName;
+      return tableRef;
+    }));
+
+    const table = new Table({
+      name: tableName,
       schemaName,
       fields: fieldsData.map(fd => fd.field),
+      // indexes, // TODO: implement Index
     });
+
+    if (pk) {
+      const field = table.fields.find(f => f.name === pk);
+      if (field) field.pk = true;
+    }
+
+    return table;
   }
 
   /*
@@ -144,7 +159,8 @@ export default class SnowflakeASTGen extends SnowflakeParserVisitor {
     }
 
     if (ctx.out_of_line_constraint()) {
-      return null;
+      const test = ctx.out_of_line_constraint().accept(this);
+      return test;
     }
 
     return null;
@@ -154,13 +170,8 @@ export default class SnowflakeASTGen extends SnowflakeParserVisitor {
   visitFull_col_decl (ctx) {
     const field = ctx.col_decl().accept(this);
 
-    // TODO: add field attribute
-
-    const inlineRefs = []; // TODO accept inline_constraint
-
     return {
       field,
-      inlineRefs,
     };
   }
 
@@ -173,5 +184,60 @@ export default class SnowflakeASTGen extends SnowflakeParserVisitor {
         schemaName: null,
       },
     });
+  }
+
+  // (CONSTRAINT id_)? (
+  //   (UNIQUE | primary_key) column_list_in_parentheses common_constraint_properties*
+  //   | foreign_key column_list_in_parentheses REFERENCES object_name column_list_in_parentheses constraint_properties
+  // )
+  visitOut_of_line_constraint (ctx) {
+    if (ctx.foreign_key()) {
+      const [databaseName, schemaName, tableName] = ctx.object_name().accept(this);
+      const sourceColumns = ctx.column_list_in_parentheses(0).accept(this);
+      const destColumns = ctx.column_list_in_parentheses(1).accept(this);
+      return {
+        kind: TABLE_CONSTRAINT_KIND.FK,
+        value: {
+          endpoints: [
+            {
+              tableName: null,
+              schemaName: null,
+              fieldNames: sourceColumns,
+              relation: '*',
+            },
+            {
+              tableName,
+              schemaName,
+              fieldNames: destColumns,
+              relation: '1',
+            },
+          ],
+        },
+      };
+    }
+    if (ctx.UNIQUE()) {
+      return {
+        kind: TABLE_CONSTRAINT_KIND.UNIQUE,
+        value: flatten(ctx.column_list_in_parentheses().map(c => c.accept(this))),
+      };
+    }
+    if (ctx.primary_key()) {
+      return {
+        kind: TABLE_CONSTRAINT_KIND.PK,
+        value: flatten(ctx.column_list_in_parentheses().map(c => c.accept(this))),
+      };
+    }
+
+    return null;
+  }
+
+  // LR_BRACKET column_list RR_BRACKET
+  visitColumn_list_in_parentheses (ctx) {
+    return ctx.column_list().accept(this);
+  }
+
+  // column_name (COMMA column_name)*
+  visitColumn_list (ctx) {
+    return ctx.column_name().map(c => getOriginalText(c));
   }
 }
