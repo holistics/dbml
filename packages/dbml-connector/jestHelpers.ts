581b0a81b1d35e0f8e46246b9349fb9f022d0bcd
huyphung1602 <huy.phung.sw@gmail.com>
2024-08-14 14:32:17 +0700

    Update bearer.yml

--- packages/dbml-connector/jestHelpers.ts
+++ packages/dbml-connector/jestHelpers.ts
@@ -1,9 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 const scanDirNames = (dirname: string, subpath: string): string[] => {
   const dirPath = path.join(dirname, subpath);
-  const dirs = fs.readdirSync(dirPath);
+  const sanitizedDirPath = path.normalize(dirPath);
+  const dirs = fs.readdirSync(sanitizedDirPath);
   return dirs;
 };
 
