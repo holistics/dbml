9bc2e392710568d357e34faa26ae7ae61816ecab
Huy Le <huy.lm@holistics.io>
2024-09-17 17:14:19 +0700

    Add more test for dbml cli after fix escape note

--- packages/dbml-cli/__test__/db2dbml/postgres/schema.sql
+++ packages/dbml-cli/__test__/db2dbml/postgres/schema.sql
@@ -132,8 +132,8 @@ CREATE TABLE table_with_comments (
 -- Add comments
 COMMENT ON TABLE table_with_comments IS 'This table stores information about various items.';
 COMMENT ON COLUMN table_with_comments.id IS 'Unique identifier for each item.';
-COMMENT ON COLUMN table_with_comments.name IS 'Name of the item.';
-COMMENT ON COLUMN table_with_comments.description IS 'Detailed description of the item.';
+COMMENT ON COLUMN table_with_comments.name IS 'Item''s name.';
+COMMENT ON COLUMN table_with_comments.description IS 'Item''s description';
 COMMENT ON COLUMN table_with_comments.created_at IS 'Timestamp when the item was created.';
 
 -- Create Authors table
