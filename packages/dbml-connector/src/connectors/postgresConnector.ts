9e2cd06421456afd34049c671f1e1859cd11bfcf
Huy Le <huy.lm@holistics.io>
2024-10-29 17:20:23 +0700

    Fix keeping table positions for Postgres connector

--- packages/dbml-connector/src/connectors/postgresConnector.ts
+++ packages/dbml-connector/src/connectors/postgresConnector.ts
@@ -120,24 +120,40 @@ const generateTablesAndFields = async (client: Client, schemas: string[]): Promi
 }> => {
   const fields: FieldsDictionary = {};
   const tablesAndFieldsSql = `
-    WITH comments AS (
-      SELECT DISTINCT ON (pc.relname, pn.nspname, pa.attname)
-        pc.relname AS table_name,
-        pn.nspname AS table_schema,
-        pa.attname AS column_name,
-        pd.description
-      FROM
-        pg_description pd
-      JOIN
-        pg_class pc ON pd.objoid = pc.oid
-      JOIN
-        pg_namespace pn ON pc.relnamespace = pn.oid
-      LEFT JOIN
-        pg_attribute pa ON pd.objoid = pa.attrelid AND pd.objsubid = pa.attnum
-      WHERE
-        pc.relkind = 'r'
-        AND pn.nspname NOT IN ('pg_catalog', 'information_schema')
-    )
+    WITH
+      comments AS (
+        SELECT DISTINCT ON (pc.relname, pn.nspname, pa.attname)
+          pc.relname AS table_name,
+          pn.nspname AS table_schema,
+          pa.attname AS column_name,
+          pd.description
+        FROM
+          pg_description pd
+        JOIN
+          pg_class pc ON pd.objoid = pc.oid
+        JOIN
+          pg_namespace pn ON pc.relnamespace = pn.oid
+        LEFT JOIN
+          pg_attribute pa ON pd.objoid = pa.attrelid AND pd.objsubid = pa.attnum
+        WHERE
+          pc.relkind = 'r'
+          AND pn.nspname NOT IN ('pg_catalog', 'information_schema')
+      ),
+      table_positions AS (
+        SELECT
+          pc.oid as table_ordinal_position,
+          pc.relname as table_name,
+          pn.nspname as table_schema
+        FROM pg_class pc
+        JOIN pg_namespace pn
+        ON pc.relnamespace = pn.oid
+        WHERE
+          -- r is ordinary table
+          -- see https://www.postgresql.org/docs/14/catalog-pg-class.html#:~:text=%3D%20temporary%20table-,relkind,-char
+          pc.relkind = 'r'
+          AND pn.nspname NOT IN ('pg_catalog', 'information_schema', 'pg_toast')
+          ${buildSchemaQuery('t.table_schema', schemas)}
+      )
     SELECT
       t.table_schema,
       t.table_name,
@@ -166,13 +182,15 @@ const generateTablesAndFields = async (client: Client, schemas: string[]): Promi
       information_schema.columns c
     JOIN
       information_schema.tables t ON c.table_name = t.table_name AND c.table_schema = t.table_schema
+    JOIN
+      table_positions tp ON tp.table_name = t.table_name AND tp.table_schema = t.table_schema
     WHERE
       t.table_type = 'BASE TABLE'
       AND t.table_schema NOT IN ('pg_catalog', 'information_schema')
       ${buildSchemaQuery('t.table_schema', schemas)}
     ORDER BY
       t.table_schema,
-      t.table_name,
+      tp.table_ordinal_position,
       c.ordinal_position
     ;
   `;
