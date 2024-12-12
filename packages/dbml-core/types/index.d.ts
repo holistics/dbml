6f91fca4f9a50a225a0c69f306951b97ef3214f1
Nguyen Hoang <anhnguyen3120@gmail.com>
2024-12-12 18:16:00 +0700

    types: update type definition for Parser.js

--- packages/dbml-core/types/index.d.ts
+++ packages/dbml-core/types/index.d.ts
@@ -2,5 +2,5 @@ import ModelExporter from './export/ModelExporter';
 import Parser from './parse/Parser';
 import importer from './import';
 import exporter from './export';
-export { importer, exporter, ModelExporter, Parser, };
-export { CompilerDiagnostic, CompilerError as CompilerDiagnostics, EditorPosition, ErrorCode, WarningLevel, } from './parse/error'
+export { importer, exporter, ModelExporter, Parser };
+export { CompilerDiagnostic, CompilerError as CompilerDiagnostics, EditorPosition, ErrorCode, WarningLevel, } from './parse/error';
