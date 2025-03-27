142632015395ac30efd75bb34271382ea0f9df8f
Huy Le <huy.lm@holistics.io>
2025-03-27 09:52:20 +0700

    add test for mssql

--- packages/dbml-core/__tests__/parser/parser.spec.js
+++ packages/dbml-core/__tests__/parser/parser.spec.js
@@ -4,10 +4,11 @@ import Parser from '../../src/parse/Parser';
 describe('@dbml/core', () => {
   describe('parser', () => {
     /**
-     * @param {string} format = [json|mysql|postgres|dbml|schemarb]
+     * @param {string} format = [json|mysql|postgres|dbml|schemarb|mssql|mssqlv2]
      */
     const runTest = (fileName, testDir, format, parseFuncName) => {
       const fileExtension = getFileExtension(format);
+
       const input = require(`./${testDir}/input/${fileName}.in.${fileExtension}`);
       const output = require(`./${testDir}/output/${fileName}.out.json`);
       const jsonSchema = Parser[parseFuncName](input);
@@ -28,6 +29,10 @@ describe('@dbml/core', () => {
 
     test.each(scanTestNames(__dirname, 'mssql-parse/input'))('mssql-parse/%s', (name) => {
       runTest(name, 'mssql-parse', 'mssql', 'parseMSSQLToJSON');
+    });
+
+    test.each(scanTestNames(__dirname, 'mssqlv2-parse/input'))('mssqlv2-parse/%s', (name) => {
+      runTest(name, 'mssqlv2-parse', 'mssql', 'parseMSSQLToJSONV2');
     });
   });
 });
