e5d5851eea35ceae244e2695bc94f0ee069751dc
Huy-DNA <huydo862003@gmail.com>
2025-10-15 17:05:09 +0700

    feat(dbml-core): support constraints in model_structure

---
+++ packages/dbml-core/src/model_structure/constraint.js
@@ -0,0 +1,51 @@
+import Element from './element';
+
+class Constraint extends Element {
+  constructor ({
+    token, name, expression, table, column = null, injectedPartial = null,
+  } = {}) {
+    super(token);
+    this.name = name;
+    this.expression = expression;
+    this.table = table;
+    this.column = column;
+    this.injectedPartial = injectedPartial;
+    this.dbState = this.table.dbState;
+    this.generateId();
+  }
+
+  generateId () {
+    this.id = this.dbState.generateId('constraintId');
+  }
+
+  export () {
+    return {
+      ...this.shallowExport(),
+    };
+  }
+
+  exportParentIds () {
+    return {
+      tableId: this.table.id,
+      columnId: this.column?.id,
+      injectedPartialId: this.injectedPartial?.id,
+    };
+  }
+
+  shallowExport () {
+    return {
+      name: this.name,
+      expression: this.expression,
+    };
+  }
+
+  normalize (model) {
+    model.constraints[this.id] = {
+      id: this.id,
+      ...this.shallowExport(),
+      ...this.exportParentIds(),
+    };
+  }
+}
+
+export default Constraint;
