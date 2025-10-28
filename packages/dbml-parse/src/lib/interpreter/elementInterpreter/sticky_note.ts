036586dcba813317a70e99b2d3a972d620690348
Huy-DNA <huydo862003@gmail.com>
2025-10-28 10:24:31 +0700

    fix(dbml-parse): change constraints to checks in interpreter

--- packages/dbml-parse/src/lib/interpreter/elementInterpreter/sticky_note.ts
+++ packages/dbml-parse/src/lib/interpreter/elementInterpreter/sticky_note.ts
@@ -1,7 +1,11 @@
 import { partition, get } from 'lodash';
 import { ElementInterpreter, InterpreterDatabase, Note } from '../types';
-import { BlockExpressionNode, ElementDeclarationNode, FunctionApplicationNode, ListExpressionNode, SyntaxNode } from '../../parser/nodes';
-import { extractColor, extractElementName, getTokenPosition, normalizeNoteContent } from '../utils';
+import {
+  BlockExpressionNode, ElementDeclarationNode, FunctionApplicationNode, ListExpressionNode, SyntaxNode,
+} from '../../parser/nodes';
+import {
+  extractColor, extractElementName, getTokenPosition, normalizeNoteContent,
+} from '../utils';
 import { CompileError, CompileErrorCode } from '../../errors';
 import { aggregateSettingList } from '../../analyzer/validator/utils';
 
@@ -10,13 +14,13 @@ export class StickyNoteInterpreter implements ElementInterpreter {
   private env: InterpreterDatabase;
   private note: Partial<Note>;
 
-  constructor(declarationNode: ElementDeclarationNode, env: InterpreterDatabase) {
+  constructor (declarationNode: ElementDeclarationNode, env: InterpreterDatabase) {
     this.declarationNode = declarationNode;
     this.env = env;
     this.note = { name: undefined, content: undefined, token: undefined };
   }
 
-  interpret(): CompileError[] {
+  interpret (): CompileError[] {
     this.note.token = getTokenPosition(this.declarationNode);
     this.env.notes.set(this.declarationNode, this.note as Note);
 
@@ -29,7 +33,7 @@ export class StickyNoteInterpreter implements ElementInterpreter {
     return errors;
   }
 
-  private interpretName(nameNode: SyntaxNode): CompileError[] {
+  private interpretName (nameNode: SyntaxNode): CompileError[] {
     const { name } = extractElementName(nameNode);
 
     this.note.name = name;
@@ -37,28 +41,27 @@ export class StickyNoteInterpreter implements ElementInterpreter {
     return [];
   }
 
-  private interpretSettingList(settings?: ListExpressionNode): CompileError[] {
+  private interpretSettingList (settings?: ListExpressionNode): CompileError[] {
     const settingMap = aggregateSettingList(settings).getValue();
 
-    this.note.headerColor = settingMap['headercolor']?.length ? extractColor(settingMap['headercolor']?.at(0)?.value as any) : undefined;
+    this.note.headerColor = settingMap.headercolor?.length ? extractColor(settingMap.headercolor?.at(0)?.value as any) : undefined;
 
     return [];
   }
 
-  private interpretBody(body: BlockExpressionNode): CompileError[] {
+  private interpretBody (body: BlockExpressionNode): CompileError[] {
     const [fields, subs] = partition(body.body, (e) => e instanceof FunctionApplicationNode);
 
     if (fields.length !== 1 || subs.length > 0) {
       return [
         new CompileError(CompileErrorCode.INVALID_NOTE, 'Invalid note syntax', body),
-      ]
+      ];
     }
 
     return [...this.interpretNote(fields[0] as FunctionApplicationNode)];
   }
 
-
-  private interpretNote(note: FunctionApplicationNode): CompileError[] {
+  private interpretNote (note: FunctionApplicationNode): CompileError[] {
     const noteContent = get(note, 'callee.expression.literal.value', '');
 
     this.note.content = normalizeNoteContent(noteContent);
