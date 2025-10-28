29ef0369c8636933fe1fb6d078710d844f014d11
Huy-DNA <huydo862003@gmail.com>
2025-10-28 10:28:47 +0700

    fix(dbml-parse): add missing files

---
+++ packages/dbml-parse/src/lib/analyzer/binder/elementBinder/checks.ts
@@ -0,0 +1,15 @@
+import ElementBinder from './elementBinder';
+
+export default class ChecksBinder extends ElementBinder {
+  protected subfield = {
+    arg: {
+      argBinderRules: [
+        {
+          shouldBind: false as const,
+        },
+      ],
+    },
+    settingList: {},
+  };
+  protected settingList = {};
+}
