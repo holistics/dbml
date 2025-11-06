79de78d3d11abd81df2cfc0197b59cae3074c4f0
Huy-DNA <huydo862003@gmail.com>
2025-11-06 10:14:24 +0700

    chore: update eslints and change dbml-cli/__test__ to dbml_cli/__tests__ for consistency

--- packages/dbml-cli/eslint.config.ts
+++ packages/dbml-cli/eslint.config.ts
@@ -1,56 +1,82 @@
-import airbnbBase from 'eslint-config-airbnb-base';
+import globals from 'globals';
+import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
-import tseslint from '@typescript-eslint/eslint-plugin';
+import airbnbBase from 'eslint-config-airbnb-base';
+import tseslint from 'typescript-eslint';
 import tsparser from '@typescript-eslint/parser';
+import jest from 'eslint-plugin-jest';
 
-export default defineConfig([
-  {
-    ignores: [
-      'node_modules/*',
-      'lib/*',
-      'jestHelpers.js',
-      'eslint.config.ts',
-    ],
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: tsparser,
-      parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 2020,
-        project: './tsconfig.json',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': tseslint,
-    },
-    rules: {
-      ...airbnbBase.rules,
-      ...tseslint.configs.recommended.rules,
-      '@typescript-eslint/no-explicit-any': 'off',
-      'no-use-before-define': 'off',
-      'no-continue': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
-      'consistent-return': 'off',
-      '@typescript-eslint/consistent-return': [
-        'error',
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  [
+    {
+      ignores: [
+        'node_modules/*',
+        'lib/*',
+        'jestHelpers.js',
+        'eslint.config.ts',
       ],
     },
-    settings: {
-      'import/resolver': {
-        typescript: {
-          alwaysTryTypes: true,
-          project: 'packages/*/{ts,js}config.json',
+    {
+      files: ['**/*.ts', '**/*.js'],
+      languageOptions: {
+        globals: {
+          ...globals.node,
+          ...globals.es2022,
+        },
+        parser: tsparser,
+        parserOptions: {
+          sourceType: 'module',
+          ecmaVersion: 2020,
+          project: './tsconfig.json',
+        },
+      },
+      rules: {
+        ...airbnbBase.rules,
+        '@typescript-eslint/no-explicit-any': 'off',
+        'no-use-before-define': 'off',
+        'no-continue': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+            caughtErrorsIgnorePattern: '^_',
+          },
+        ],
+        'consistent-return': 'off',
+        '@typescript-eslint/consistent-return': [
+          'error',
+        ],
+      },
+      settings: {
+        'import/resolver': {
+          typescript: {
+            alwaysTryTypes: true,
+            project: 'packages/*/{ts,js}config.json',
+          },
         },
       },
     },
-  },
-]);
+    {
+      files: ['**/*.test.js', '**/*.spec.js'],
+      ...jest.configs['flat/recommended'],
+      languageOptions: {
+        globals: {
+          ...jest.environments.globals.globals,
+        },
+      },
+      rules: {
+        'no-undef': 'warn',
+      },
+    },
+    {
+      files: ['bin/**/*', '**/*.test.js', '**/*.spec.js', '__tests__/**/*'],
+      rules: {
+        '@typescript-eslint/no-require-imports': 'warn',
+        'no-global-assign': 'warn',
+      },
+    },
+  ],
+);
--- packages/dbml-core/eslint.config.ts
+++ packages/dbml-core/eslint.config.ts
@@ -1,18 +1,58 @@
-import airbnbBase from 'eslint-config-airbnb-base';
+import jest from 'eslint-plugin-jest';
+import globals from 'globals';
+import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
+import airbnbBase from 'eslint-config-airbnb-base';
 
-export default defineConfig([
-  {
-    ignores: [
-      'node_modules/*',
-      'lib/*',
-      'types/*',
-    ],
-  },
-  {
-    files: ['**/*.js'],
-    rules: {
-      ...airbnbBase.rules,
-    }
-  },
-]);
+export default defineConfig(
+  eslint.configs.recommended,
+  [
+    {
+      ignores: [
+        'node_modules/*',
+        'lib/*',
+        'types/*',
+        'src/parse/dbmlParser.js',
+        'src/parse/mssqlParser.js',
+        'src/parse/mysqlParser.js',
+        'src/parse/postgresParser.js',
+        'src/parse/schemarbParser.js',
+        'src/parse/ANTLR/parsers/*',
+      ],
+    },
+    {
+      files: ['**/*.js'],
+      languageOptions: {
+        globals: {
+          ...globals.browser,
+          ...globals.jest,
+          ...globals.node,
+          ...globals.es2022,
+        },
+      },
+      rules: {
+        ...airbnbBase.rules,
+        'no-unused-vars': [
+          'warn',
+          {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+            caughtErrorsIgnorePattern: '^_',
+          },
+        ],
+      }
+    },
+    {
+      files: ['**/__tests__/**/*.js', '**/*.test.js', '**/*.spec.js'],
+      ...jest.configs['flat/recommended'],
+      languageOptions: {
+        globals: {
+          ...jest.environments.globals.globals,
+        },
+      },
+      rules: {
+        'no-undef': 'warn',
+      },
+    },
+  ],
+);
