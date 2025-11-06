e9e77ad907d2c8ed14883fd9bb40459c9756071b
Huy-DNA <huydo862003@gmail.com>
2025-11-06 11:52:01 +0700

    chore(dbml-cli): move "jest" in package.json to jest.config.ts

--- packages/dbml-cli/jest.config.ts
+++ packages/dbml-cli/jest.config.ts
@@ -2,6 +2,12 @@ import { type Config } from 'jest';
 
 const config: Config = {
   testMatch: ["**/?(*.)+(spec|test).?([mc])[jt]s?(x)"],
+  setupFiles: [
+    "./jestHelpers.js",
+  ],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
 };
 
 export default config;
