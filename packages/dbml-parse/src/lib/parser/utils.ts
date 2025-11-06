a593b726e56d0692ded54f6fe3b460ca1401a800
Huy-DNA <huydo862003@gmail.com>
2025-11-06 16:14:44 +0700

    feat(dbml-parse): allow enum value in column default in validator

--- packages/dbml-parse/src/lib/parser/utils.ts
+++ packages/dbml-parse/src/lib/parser/utils.ts
@@ -345,17 +345,28 @@ export function isExpressionAnIdentifierNode (value?: unknown): value is Primary
   );
 }
 
-export function isAccessExpression (node: SyntaxNode): node is InfixExpressionNode & {
+type AccessExpression = InfixExpressionNode & {
   leftExpression: SyntaxNode;
   rightExpression: SyntaxNode;
   op: SyntaxToken & { value: '.' };
-} {
+};
+
+type DotDelimitedIdentifier = PrimaryExpressionNode | (AccessExpression & {
+  rightExpression: AccessExpression | PrimaryExpressionNode;
+});
+
+export function isAccessExpression (node: SyntaxNode): node is AccessExpression {
   return (
     node instanceof InfixExpressionNode
     && node.leftExpression instanceof SyntaxNode
     && node.rightExpression instanceof SyntaxNode
     && node.op?.value === '.'
   );
+}
+
+export function isDotDelimitedIdentifier (node: SyntaxNode): node is DotDelimitedIdentifier {
+  if (isExpressionAVariableNode(node)) return true;
+  return isAccessExpression(node) && isExpressionAVariableNode(node.leftExpression) && isDotDelimitedIdentifier(node.rightExpression);
 }
 
 export function extractStringFromIdentifierStream (stream?: IdentiferStreamNode): Option<string> {
