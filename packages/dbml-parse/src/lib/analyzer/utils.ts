fa6b84c5bc7ebc3a03c1bcdc1e3ae6589fa6db73
Huy-DNA <huydo862003@gmail.com>
2025-11-12 10:42:08 +0700

    fix(dbml-parse): remove all references to PartialInjectionNode

--- packages/dbml-parse/src/lib/analyzer/utils.ts
+++ packages/dbml-parse/src/lib/analyzer/utils.ts
@@ -4,7 +4,6 @@ import {
   ElementDeclarationNode,
   FunctionExpressionNode,
   InfixExpressionNode,
-  PartialInjectionNode,
   PrimaryExpressionNode,
   ProgramNode,
   SyntaxNode,
@@ -151,11 +150,6 @@ export function extractVarNameFromPrimaryVariable (
 ): Option<string> {
   const value = node?.expression.variable?.value;
 
-  return value === undefined ? new None() : new Some(value);
-}
-
-export function extractVarNameFromPartialInjection (node?: PartialInjectionNode): Option<string> {
-  const value = node?.partial?.variable?.value;
   return value === undefined ? new None() : new Some(value);
 }
 
