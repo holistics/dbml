00be264a25b87f15450754afb4dcfc0997d0028b
Huy-DNA <huydo862003@gmail.com>
2025-11-17 21:09:01 +0700

    test(dbml-core): prune some source files from coverage

--- packages/dbml-core/jest.config.ts
+++ packages/dbml-core/jest.config.ts
@@ -22,6 +22,21 @@ const config: Config = {
   collectCoverage: true,
   coverageReporters: ['json', 'json-summary'],
   coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!src/parse/buildParser.js',
+    '!src/parse/dbmlParser.js',
+    '!src/parse/mssqlParser.js',
+    '!src/parse/mysqlParser.js',
+    '!src/parse/postgresParser.js',
+    '!src/parse/schemarbParser.js',
+    '!src/parse/dbml/**/*.js',
+    '!src/parse/mssql/**/*.js',
+    '!src/parse/mysql/**/*.js',
+    '!src/parse/postgresql/**/*.js',
+    '!src/parse/schemarb/**/*.js',
+    '!src/parse/ANTLR/parsers/**/*.js',
+  ],
 };
 
 export default config;
