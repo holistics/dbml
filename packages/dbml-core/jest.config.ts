0a80c0784807570b334b86966b5a8949046ef7ee
Huy-DNA <huydo862003@gmail.com>
2025-11-17 14:59:17 +0700

    chore: add coverage to all packages and setup coverage report workflow

--- packages/dbml-core/jest.config.ts
+++ packages/dbml-core/jest.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
     '^lodash-es$': 'lodash',
   },
   collectCoverage: true,
-  coverageReporters: ["json"],
+  coverageReporters: ["json", "json-summary"],
   coverageDirectory: "coverage",
 };
 
