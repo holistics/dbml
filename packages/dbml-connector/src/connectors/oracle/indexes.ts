ea95a6efa6c2b82622282e8fc35d89aff67bf1f8
Huy-DNA <huydo862003@gmail.com>
2025-11-27 21:45:08 +0700

    fix: exclude system indexes and increment default

--- packages/dbml-connector/src/connectors/oracle/indexes.ts
+++ packages/dbml-connector/src/connectors/oracle/indexes.ts
@@ -1,92 +1,68 @@
 import { Connection } from 'oracledb';
 import { Index, IndexesDictionary } from '../types';
-import { zip } from 'lodash';
-import { LIST_SEPARATOR } from './utils';
+import { EXECUTE_OPTIONS } from './utils';
 
 export async function generateIndexes (client: Connection): Promise<IndexesDictionary> {
   const query = `
-    WITH indexes AS (
-        SELECT 
-            i.INDEX_NAME AS index_name,
-            i.TABLE_NAME AS table_name,
-            CASE
-              WHEN i.INDEX_TYPE LIKE '%NORMAL' THEN 'btree' -- btree index is stored as 'NORMAL' or 'FUNCTION-BASED NORMAL'
-              ELSE LOWER(REGEXP_REPLACE(i.INDEX_TYPE, '^FUNCTION-BASED ', ''))
-            END AS index_type,
-            CASE
-              WHEN i.UNIQUENESS = 'UNIQUE' THEN 1
-              ELSE 0
-            END AS is_unique
-        FROM USER_INDEXES i
-        WHERE NOT EXISTS (
-            SELECT 1 
-            FROM USER_CONSTRAINTS c
-            WHERE c.INDEX_NAME = i.INDEX_NAME -- Some primary and unique constraints are backed by an index, we don't include these indexes again to avoid duplication
-        )
-    ),
-    index_columns_and_expressions AS (
-        SELECT 
-            ic.INDEX_NAME AS index_name,
-            ic.COLUMN_NAME AS column,
-            ic.COLUMN_POSITION AS column_position,
-            'column' AS column_type
-        FROM USER_IND_COLUMNS ic
-        
-        UNION ALL
-
-        SELECT 
-            ie.INDEX_NAME AS index_name,
-            ie.COLUMN_EXPRESSION AS column,
-            ie.COLUMN_POSITION AS column_position,
-            'expression' AS column_type
-        FROM USER_IND_EXPRESSIONS ie
-    ),
-    index_columns_by_name AS (
-        SELECT 
-            ice.index_name AS index_name,
-            LISTAGG(ice.column, '${LIST_SEPARATOR}') WITHIN GROUP (ORDER BY ice.column_position) AS columns,
-            LISTAGG(ice.column_type, '${LIST_SEPARATOR}') WITHIN GROUP (ORDER BY ice.column_position) AS column_types
-        FROM index_columns_and_expressions ice
-        GROUP BY ice.INDEX_NAME
+    SELECT
+      i.INDEX_NAME,
+      i.TABLE_NAME,
+      CASE
+        WHEN i.INDEX_TYPE LIKE '%NORMAL' THEN 'btree'
+        ELSE LOWER(REGEXP_REPLACE(i.INDEX_TYPE, '^FUNCTION-BASED ', ''))
+      END AS INDEX_TYPE,
+      CASE WHEN i.UNIQUENESS = 'UNIQUE' THEN 1 ELSE 0 END AS IS_UNIQUE,
+      ic.COLUMN_NAME,
+      ic.COLUMN_POSITION,
+      ie.COLUMN_EXPRESSION
+    FROM USER_INDEXES i
+    LEFT JOIN USER_IND_COLUMNS ic
+      ON i.INDEX_NAME = ic.INDEX_NAME
+    LEFT JOIN USER_IND_EXPRESSIONS ie
+      ON i.INDEX_NAME = ie.INDEX_NAME
+      AND ic.COLUMN_POSITION = ie.COLUMN_POSITION
+    WHERE NOT EXISTS (
+      SELECT 1
+      FROM USER_CONSTRAINTS c
+      WHERE c.INDEX_NAME = i.INDEX_NAME
     )
-    SELECT 
-        i.index_name,
-        i.table_name,
-        icn.columns,
-        icn.column_types,
-        i.index_type,
-        i.is_unique
-    FROM indexes i
-    LEFT JOIN index_columns_by_name icn
-        ON i.index_name = icn.index_name
+    ORDER BY i.INDEX_NAME, ic.COLUMN_POSITION
   `;
 
+  const res = await client.execute(query, [], EXECUTE_OPTIONS);
+
   const indexes: IndexesDictionary = {};
-  const res = await client.execute(query);
+  const indexMap: Record<string, { table: string; type: string; unique: boolean; columns: Array<{ type: 'column' | 'expression'; value: string }> }> = {};
+
   res.rows?.forEach((row) => {
-    const { index_name, is_unique, table_name, columns: _columns, column_types, index_type } = row as Record<string, unknown>;
-    const indexName = index_name as string || '';
-    const isUnique = is_unique as boolean || false;
-    const tableName = table_name as string || '';
-    const columns = _columns as string || '';
-    const columnTypes = column_types as string || '';
-    const indexType = index_type as string || '';
+    const { INDEX_NAME, TABLE_NAME, INDEX_TYPE, IS_UNIQUE, COLUMN_NAME, COLUMN_EXPRESSION } = row as Record<string, unknown>;
 
-    const untypedColumnList = columns.split(LIST_SEPARATOR);
-    const typeList = columnTypes.split(LIST_SEPARATOR);
-    const zippedColumnList = zip(untypedColumnList, typeList);
+    const name = INDEX_NAME as string;
+    const table = TABLE_NAME as string;
+    const type = INDEX_TYPE as string;
+    const unique = !!(IS_UNIQUE as number);
 
-    const index: Index = {
-      name: indexName,
-      type: indexType,
-      unique: isUnique,
-      columns: zippedColumnList.map((c) => ({
-        type: c[1] as 'column' | 'expression',
-        value: c[0]!,
-      })),
-    };
-    if (!indexes[tableName]) indexes[tableName] = [];
-    indexes[tableName].push(index);
+    if (COLUMN_EXPRESSION) {
+      if (!indexMap[name]) {
+        indexMap[name] = { table, type, unique, columns: [] };
+      }
+      indexMap[name].columns.push({ type: 'expression', value: COLUMN_EXPRESSION as string });
+      return;
+    }
+    if (COLUMN_NAME) {
+      if (!indexMap[name]) {
+        indexMap[name] = { table, type, unique, columns: [] };
+      }
+      indexMap[name].columns.push({ type: 'column', value: COLUMN_NAME as string });
+      return;
+    }
   });
+
+  Object.entries(indexMap).forEach(([name, { table, type, unique, columns }]) => {
+    const index: Index = { name, type, unique, columns };
+    if (!indexes[table]) indexes[table] = [];
+    indexes[table].push(index);
+  });
+
   return indexes;
 }
--- packages/dbml-connector/src/connectors/oracle/utils.ts
+++ packages/dbml-connector/src/connectors/oracle/utils.ts
@@ -1,1 +1,5 @@
+import oracledb from 'oracledb';
+
 export const LIST_SEPARATOR = '@@@@@@@@'; // Used to join multiple fields into a string field to simulate array column types
+
+export const EXECUTE_OPTIONS = { outFormat: oracledb.OUT_FORMAT_OBJECT };
