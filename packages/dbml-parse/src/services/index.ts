9c3aed1c71ce07a80562c373724bb464f9f1bdf1
Huy-DNA <huydo862003@gmail.com>
2026-01-15 23:27:00 +0700

    Revert "feat: add basic hover provider"
    This reverts commit 3803d0ebaa89e0fafe9b75090b36c32959608c2a.

--- packages/dbml-parse/src/services/index.ts
+++ packages/dbml-parse/src/services/index.ts
@@ -1,7 +1,6 @@
 import DBMLCompletionItemProvider from './suggestions/provider';
 import DBMLDefinitionProvider from './definition/provider';
 import DBMLReferencesProvider from './references/provider';
-import DBMLHoverProvider from './hover/provider';
 
 export * from '@/services/types';
 
@@ -9,5 +8,4 @@ export {
   DBMLCompletionItemProvider,
   DBMLDefinitionProvider,
   DBMLReferencesProvider,
-  DBMLHoverProvider,
 };
