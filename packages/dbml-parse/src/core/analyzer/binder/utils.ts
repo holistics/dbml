1cff84ff9ddf24d7a5ac8855fcb9055441b8e5fb
Huy-DNA <huydo862003@gmail.com>
2026-01-18 22:37:17 +0700

    chore: lint and rename

--- packages/dbml-parse/src/core/analyzer/binder/utils.ts
+++ packages/dbml-parse/src/core/analyzer/binder/utils.ts
@@ -14,7 +14,7 @@ import TablePartialBinder from './elementBinder/tablePartial';
 import { destructureComplexVariableTuple, extractVarNameFromPrimaryVariable } from '@/core/analyzer/utils';
 import { SymbolKind, createNodeSymbolIndex } from '@/core/analyzer/symbol/symbolIndex';
 import { getSymbolKind } from '@/core/analyzer/symbol/utils';
-import { getElementName, isExpressionAVariableNode } from '@/core/parser/utils';
+import { getElementNameString, isExpressionAVariableNode } from '@/core/parser/utils';
 import { CompileError, CompileErrorCode } from '@/core/errors';
 import { DEFAULT_SCHEMA_NAME } from '@/constants';
 import RecordsBinder from './elementBinder/records';
@@ -98,7 +98,7 @@ export function lookupAndBindInScope (
 
   let curSymbolTable = initialScope.symbol.symbolTable;
   let curKind = getSymbolKind(initialScope.symbol);
-  let curName = initialScope instanceof ElementDeclarationNode ? getElementName(initialScope).unwrap_or('<invalid name>') : DEFAULT_SCHEMA_NAME;
+  let curName = initialScope instanceof ElementDeclarationNode ? getElementNameString(initialScope).unwrap_or('<invalid name>') : DEFAULT_SCHEMA_NAME;
 
   if (initialScope instanceof ProgramNode && symbolInfos.length) {
     const { node, kind } = symbolInfos[0];
