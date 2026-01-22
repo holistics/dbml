00996045bdf2b914b682dd06fd7aad6d133b406f
Huy-DNA <huydo862003@gmail.com>
2026-01-22 09:25:33 +0700

    feat: export the value formatter

--- packages/dbml-core/types/index.d.ts
+++ packages/dbml-core/types/index.d.ts
@@ -5,3 +5,5 @@ import exporter from './export';
 import { renameTable } from './transform';
 export { renameTable, importer, exporter, ModelExporter, Parser };
 export { CompilerDiagnostic, CompilerError as CompilerDiagnostics, EditorPosition, ErrorCode, WarningLevel, } from './parse/error';
+export { formatRecordValue, RecordValue } from './export';
+export { RecordValueType } from './model_structure/database';
