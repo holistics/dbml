d18352e08cee2924755bf6e5c5de24cad6201c67
Huy-DNA <huydo862003@gmail.com>
2026-01-26 12:43:27 +0700

    chore: remove unused records queries

--- packages/dbml-core/types/transform/index.d.ts
+++ packages/dbml-core/types/transform/index.d.ts
@@ -1,42 +1,7 @@
-import { RecordValue } from '../model_structure/database';
-
 export type TableNameInput = string | { schema?: string; table: string };
 
 export function renameTable(
   oldName: TableNameInput,
   newName: TableNameInput,
-  dbmlCode: string
-): string;
-
-export function appendRecords(
-  tableName: TableNameInput,
-  columns: string[],
-  values: Array<Array<RecordValue | string | number | boolean | null>>,
-  dbmlCode: string
-): string;
-
-export function updateRecordField(
-  tableName: TableNameInput,
-  rowIndex: number,
-  fieldName: string,
-  newValue: RecordValue | string | number | boolean | null,
-  dbmlCode: string
-): string;
-
-export function deleteRecordRow(
-  tableName: TableNameInput,
-  rowIndex: number,
-  dbmlCode: string
-): string;
-
-export function deleteRecordValue(
-  tableName: TableNameInput,
-  rowIndex: number,
-  columnName: string,
-  dbmlCode: string
-): string;
-
-export function removeAllRecords(
-  tableName: TableNameInput,
   dbmlCode: string
 ): string;
