5b4a3b1abeec7ed26f67a88bead5b19651a6ebd2
Huy-DNA <huydo862003@gmail.com>
2026-01-26 12:43:27 +0700

    chore: remove unused records queries

--- packages/dbml-parse/src/compiler/queries/transform/index.ts
+++ packages/dbml-parse/src/compiler/queries/transform/index.ts
@@ -1,10 +1,3 @@
 export { renameTable } from './renameTable';
 export { applyTextEdits, type TextEdit } from './applyTextEdits';
 export { type TableNameInput } from './utils';
-export {
-  appendRecords,
-  updateRecordField,
-  deleteRecordRow,
-  deleteRecordValue,
-  removeAllRecords,
-} from './records';
