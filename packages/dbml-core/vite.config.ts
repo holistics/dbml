4af037eaccb74d7cebc92db1a500ae92695227b1
Huy-DNA <huydo862003@gmail.com>
2026-02-05 10:38:18 +0700

    chore: remove redundant config in vite.config.ts

--- packages/dbml-core/vite.config.ts
+++ packages/dbml-core/vite.config.ts
@@ -17,9 +17,6 @@ export default defineConfig({
       fileName: 'index',
       formats: ['cjs', 'es'],
     },
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
   },
   optimizeDeps: {
     exclude: ['parsimmon'],
