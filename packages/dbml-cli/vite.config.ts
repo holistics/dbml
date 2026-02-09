783855db50e38905de540eb043ee9a3346590b36
Huy-DNA <huydo862003@gmail.com>
2026-02-09 13:17:16 +0700

    fix: use single quotes in vite.config.ts for consistency

--- packages/dbml-cli/vite.config.ts
+++ packages/dbml-cli/vite.config.ts
@@ -1,4 +1,4 @@
-/// <reference types="vitest" />
+/// <reference types='vitest' />
 
 import path from 'path';
 import { defineConfig } from 'vite';
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src/"),
+      '@': path.resolve(__dirname, 'src/'),
     },
   },
   build: {
@@ -14,7 +14,7 @@ export default defineConfig({
     outDir: 'lib',
     minify: false,
     lib: {
-      entry: path.resolve(__dirname, "src/index.js"),
+      entry: path.resolve(__dirname, 'src/index.js'),
       fileName: 'index',
       formats: ['cjs'],
     },
