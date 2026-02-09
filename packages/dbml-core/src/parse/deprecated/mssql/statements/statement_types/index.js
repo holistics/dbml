7ac3b8730a17de140202e60f5ed451ee18782c17
Huy DNA <huydo862003@gmail.com>
2026-02-09 13:37:41 +0700

    Update packages/dbml-core/src/parse/deprecated/mssql/statements/statement_types/index.js
    Co-authored-by: Nguyen Hoang <anhnguyen3120@gmail.com>

--- packages/dbml-core/src/parse/deprecated/mssql/statements/statement_types/index.js
+++ packages/dbml-core/src/parse/deprecated/mssql/statements/statement_types/index.js
@@ -1,11 +1,5 @@
-import pAlterTable from './alter_table/index.js';
-import pCreateIndex from './create_index/index.js';
-import pCreateTable from './create_table/index.js';
-import pCommentSyntax from './comments/index.js';
+export { default as pAlterTable } from './alter_table/index.js';
+export { default as pCreateIndex } from './create_index/index.js';
+export { default as pCreateTable } from './create_table/index.js';
+export { default as pCommentSyntax } from './comments/index.js';
 
-export {
-  pAlterTable,
-  pCreateIndex,
-  pCreateTable,
-  pCommentSyntax,
-};
