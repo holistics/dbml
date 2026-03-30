326918319f49a5589d10b7ca2565a09577f49bc9
Huy-DNA <huydo862003@gmail.com>
2026-03-30 17:12:41 +0700

    refactor: remove getIntern/setIntern, update snapshots to drop duplicate-declaration symbol entries

--- packages/dbml-parse/src/core/internable.ts
+++ packages/dbml-parse/src/core/internable.ts
@@ -48,15 +48,6 @@ export class InternedMap<K extends Internable<P>, V, P extends Primitive = Retur
     return this.map.size;
   }
 
-  getIntern (key: P): V | undefined {
-    return this.map.get(key);
-  }
-
-  setIntern (key: P, value: V): this {
-    this.map.set(key, value);
-    return this;
-  }
-
   entries (): IterableIterator<[P, V]> {
     return this.map[Symbol.iterator]();
   }
