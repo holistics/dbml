249fc4e6b4f3040148f23bf6cdb6dafd7a195c7d
Huy-DNA <huydo862003@gmail.com>
2026-03-31 16:55:59 +0700

    refactor: migrate transform

--- packages/dbml-core/types/index.d.ts
+++ packages/dbml-core/types/index.d.ts
@@ -2,11 +2,7 @@ import ModelExporter from './export/ModelExporter';
 import Parser from './parse/Parser';
 import importer from './import';
 import exporter from './export';
-import {
-  renameTable,
-} from './transform';
 export {
-  renameTable,
   importer,
   exporter,
   ModelExporter,
--- packages/dbml-core/types/transform/index.d.ts
+++
@@ -1,7 +0,0 @@
-export type TableNameInput = string | { schema?: string; table: string };
-
-export function renameTable(
-  oldName: TableNameInput,
-  newName: TableNameInput,
-  dbmlCode: string
-): string;
