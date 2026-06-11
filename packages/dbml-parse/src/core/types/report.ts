add32ffd6843345476585c3b530791e60d745b55
Huy-DNA <huydo862003@gmail.com>
2026-06-11 18:03:31 +0700

    fix: avoid unnecessary allocation for a simple function like getFiltered

--- packages/dbml-parse/src/core/types/report.ts
+++ packages/dbml-parse/src/core/types/report.ts
@@ -31,8 +31,8 @@ export default class Report<T> {
 
   // Extract the reported value
   // If the reported value is one of filteredValue, return undefined
-  getFiltered<S extends symbol | undefined | null> (...filteredValues: S[]): Exclude<T, S> | undefined {
-    return filteredValues.includes(this.value as any) ? undefined : this.value as Exclude<T, S>;
+  getFiltered<S extends symbol | undefined | null> (filteredValue: S): Exclude<T, S> | undefined {
+    return this.value as any === filteredValue ? undefined : this.value as Exclude<T, S>;
   }
 
   getValue (): T {
@@ -51,8 +51,8 @@ export default class Report<T> {
   // 1. Transform the reported value via `fn`
   // 2. If `fn` produces further warnings or errors, accumulate
   // If the reported value is one of filteredValue, return undefined
-  chainFiltered<S extends symbol | undefined | null, U>(fn: (_: Exclude<T, S>) => Report<U>, ...filteredValues: S[]): Report<U | undefined> {
-    if (filteredValues.includes(this.value as any)) return new Report(undefined, this.errors, this.warnings);
+  chainFiltered<S extends symbol | undefined | null, U>(fn: (_: Exclude<T, S>) => Report<U>, filteredValue: S): Report<U | undefined> {
+    if (this.value as any === filteredValue) return new Report(undefined, this.errors, this.warnings);
     const res = fn(this.value as Exclude<T, S>);
     const errors = [
       ...this.errors,
@@ -84,8 +84,8 @@ export default class Report<T> {
   // 1. Transform the reported value via `fn`
   // 2. `fn` cannot produce further warnings or errors
   // If the reported value is one of filteredValue, return undefined
-  mapFiltered<S extends symbol | undefined | null, U>(fn: (_: Exclude<T, S>) => U, ...filteredValues: S[]): Report<U | undefined> {
-    if (filteredValues.includes(this.value as any)) return new Report(undefined, this.errors, this.warnings);
+  mapFiltered<S extends symbol | undefined | null, U>(fn: (_: Exclude<T, S>) => U, filteredValue: S): Report<U | undefined> {
+    if (this.value as any === filteredValue) return new Report(undefined, this.errors, this.warnings);
     return new Report<U>(fn(this.value as Exclude<T, S>), this.errors, this.warnings);
   }
 
