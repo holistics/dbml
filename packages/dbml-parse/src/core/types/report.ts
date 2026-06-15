0ce0b622c785aa69060232623ed4ec2654c07b61
Huy-DNA <huydo862003@gmail.com>
2026-06-15 15:20:48 +0700

    doc: update comments

--- packages/dbml-parse/src/core/types/report.ts
+++ packages/dbml-parse/src/core/types/report.ts
@@ -30,7 +30,7 @@ export default class Report<T> {
   }
 
   // Extract the reported value
-  // If the reported value is one of filteredValue, return undefined
+  // If the reported value is filteredValue, return undefined
   getFiltered<S extends symbol | undefined | null> (filteredValue: S): Exclude<T, S> | undefined {
     return this.value as any === filteredValue ? undefined : this.value as Exclude<T, S>;
   }
@@ -50,7 +50,7 @@ export default class Report<T> {
   // Chain the reported value
   // 1. Transform the reported value via `fn`
   // 2. If `fn` produces further warnings or errors, accumulate
-  // If the reported value is one of filteredValue, return undefined
+  // If the reported value is filteredValue, return undefined
   chainFiltered<S extends symbol | undefined | null, U>(fn: (_: Exclude<T, S>) => Report<U>, filteredValue: S): Report<U | undefined> {
     if (this.value as any === filteredValue) return new Report(undefined, this.errors, this.warnings);
     const res = fn(this.value as Exclude<T, S>);
@@ -83,7 +83,7 @@ export default class Report<T> {
   // Map the reported value
   // 1. Transform the reported value via `fn`
   // 2. `fn` cannot produce further warnings or errors
-  // If the reported value is one of filteredValue, return undefined
+  // If the reported value is filteredValue, return undefined
   mapFiltered<S extends symbol | undefined | null, U>(fn: (_: Exclude<T, S>) => U, filteredValue: S): Report<U | undefined> {
     if (this.value as any === filteredValue) return new Report(undefined, this.errors, this.warnings);
     return new Report<U>(fn(this.value as Exclude<T, S>), this.errors, this.warnings);
